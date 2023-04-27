28ecdb9898e26e3b28af801327a083459938defb
spacegoing <spacegoing@gmail.com>
2023-04-27 17:59:57 +0000

    [New] replicate chunked differences

--- examples/learn_pmm.py
+++ examples/learn_pmm.py
@@ -1,9 +1,17 @@
+import numpy as np
+import random
+import pandas as pd
 from numba import njit
 from hftbacktest import HftBacktest, FeedLatency, SquareProbQueueModel, Linear, Stat, BUY, SELL, GTX
 
+seed = 23
+# Set seed for NumPy
+np.random.seed(seed)
+# Set seed for Pandas
+random.seed(seed)
+
 
 #* Strategy
-@njit
 def simple_two_sided_quote(hbt, stat):
   max_position = 5
   half_spread = hbt.tick_size * 20
@@ -14,7 +22,7 @@ def simple_two_sided_quote(hbt, stat):
 
   while hbt.run:
     # Check every 0.1s
-    if not hbt.elapse(0.1 * 1e6):
+    if not hbt.elapse(1e5):
       return False
 
     # Clear cancelled, filled or expired orders.
@@ -64,24 +72,102 @@ def simple_two_sided_quote(hbt, stat):
   return True
 
 
-#* Backtest
+# Backtest
 # This backtest assumes market maker rebates.
 # https://www.binance.com/kz/support/announcement/binance-upgrades-usd%E2%93%A2-margined-futures-liquidity-provider-program-2023-04-04-01007356e6514df3811b0c80ab8c83bf
 
-hbt = HftBacktest([
-    'btcusdt_20230405.npz',
-],
-                  tick_size=0.01,
-                  lot_size=0.001,
-                  maker_fee=-0.00005,
-                  taker_fee=0.0007,
-                  order_latency=FeedLatency(),
-                  queue_model=SquareProbQueueModel(),
-                  asset_type=Linear,
-                  snapshot='btcusdt_20230404_eod.npz')
+hbt = HftBacktest(
+    [
+        'test_chunks/btcusdt_20230405_0.npz',
+        'test_chunks/btcusdt_20230405_1.npz',
+        'test_chunks/btcusdt_20230405_2.npz',
+        'test_chunks/btcusdt_20230405_3.npz',
+        'test_chunks/btcusdt_20230405_4.npz',
+        'test_chunks/btcusdt_20230405_5.npz',
+        'test_chunks/btcusdt_20230405_6.npz',
+    ],
+    # hbt = HftBacktest([
+    #     'btcusdt_20230405.npz',
+    # ],
+    tick_size=0.01,
+    lot_size=0.001,
+    maker_fee=-0.00005,
+    taker_fee=0.0007,
+    order_latency=FeedLatency(),
+    queue_model=SquareProbQueueModel(),
+    asset_type=Linear,
+    snapshot='btcusdt_20230404_eod.npz')
 
 stat = Stat(hbt)
 simple_two_sided_quote(hbt, stat.recorder)
 
-#* Viz
+# Viz
 stat.summary(capital=2000)
+
+if __name__ == "__main__":
+  #* Debug
+  a = np.load('chunked_balance.npy')
+  unequal_indices = np.argwhere(a != stat.balance)
+  first_unequal_index = unequal_indices[0, 0]
+  print(first_unequal_index)
+
+  pos = np.load('o_position.npy')
+  unequal_indices = np.argwhere(pos != stat.position)
+  first_unequal_index = unequal_indices[0, 0]
+  print(first_unequal_index)
+
+  # dt = stat.datetime()
+  # dt[1478]
+  # Out[14]: 1680653149332808
+  # Out[18]: Timestamp('2023-04-05 00:05:49.332808+0000', tz='UTC')
+
+  # For chunked timestamp
+  # In [11]: stat.timestamp[1478]
+  # Out[11]: 1680653149379907
+
+  row = 0
+  for i in range(7):
+    filename = f'test_chunks/btcusdt_20230405_{i}.npz'
+    data = np.load(filename)['data']
+    print(filename)
+    date = np.where(data[:, 1] > 0, data[:, 1], data[:, 2])
+    unequal_indices = np.argwhere(date.astype(int) > 1680653112209163)
+    if np.size(unequal_indices) == 0:
+      print('Not found !!!')
+      row += data.shape[0]
+      # 218450 + 263
+    else:
+      first_unequal_index = unequal_indices[0, 0]
+      print('FFFFFFFFFFFFFFFFF')
+      print(first_unequal_index)
+
+  cked_5_diff = data[263 - 5:263 + 5, :]
+  np.save('slice_5.npy', cked_5_diff)
+
+  d = np.load('btcusdt_20230405.npz')['data']
+  idx = 218450 + 263
+  td = d[idx - 5:idx + 5, :]
+
+  idx = 17400
+  date = np.where(data[idx, 1] > 0, data[idx, 1], data[idx, 2])
+  t = pd.to_datetime(date, utc=True, unit='us')
+  print(t)
+
+#* debug timestamp
+ots = np.load('o_ts.npy')
+# np.save('o_ts.npy', ots)
+cts = np.array(stat.timestamp)
+# np.save('c_ts.npy', cts)
+unequal_indices = np.argwhere(cts != ots)
+
+idx = unequal_indices[0, 0]
+print(idx)
+# 1324
+print(ots[idx], cts[idx])
+# 1680653112209163 1680653112213197
+# 2023-04-05 00:05:12.209163+00:00
+# 2023-04-05 00:05:12.213197+00:00
+# print(pd.to_datetime(1680653112209163 , utc=True, unit='us'))
+print(ots[idx - 5:idx + 5])
+print(cts[idx - 5:idx + 5])
+
